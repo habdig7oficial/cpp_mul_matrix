025e7523483711208aa40e962468c027c150de67
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-04-10 13:02:11 -0300

    correct partitioner

--- mul.cpp
+++ mul.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 template <typename Number>
-
 Number *mul_matrix(Number *matrix1, int lenx1, int leny1, Number *matrix2, int lenx2, int leny2){
   if(lenx1 != leny2)
     throw runtime_error("Invalid multiplication");
@@ -27,8 +26,31 @@ Number *mul_matrix(Number *matrix1, int lenx1, int leny1, Number *matrix2, int l
   return matrix_res;
 }
 
+
+template <typename Number>
+Number *markov_step(uint target, Number *chain, int len, uint step, uint step_b,  bool *success){
+  if(step == target){
+    *success = true;
+    return chain;
+  }
+  else if(step > target || step  == 0){
+    *success = false;
+    return chain;
+  }
+
+  cout << step << " " << step + step_b << endl;
+
+  Number *res = markov_step(target, chain, len, 2 * step, step, success);
+  
+  if(*success)
+    return res;
+  else
+    return  markov_step(target, chain, len, step + step_b, step_b / 2, success);;
+}
+
+
 int main(){
-  /*
+  
   double a[][DIMENSIONS] = {
     {0.1, 0.2, 0.7},
     {0.9, 0.1, 0},
@@ -40,9 +62,9 @@ int main(){
      {2.0, 1.0},
      {1.0, 2.0},
      {0.1, 1.0}
---  };
+  };
 
-  */
+   /* 
 
     double a[][DIMENSIONS] = {
       {2, 3, 1},
@@ -56,7 +78,7 @@ int main(){
       {0, 5},
       {4, 1.0}
      };
-  
+   */
 
    int sizex_a = sizeof(a[0]) / sizeof(double);
    int sizex_b = sizeof(b[0]) / sizeof(double);
@@ -64,13 +86,28 @@ int main(){
    int sizey_a = sizeof(a) / sizeof(a[0]);
    int sizey_b = sizeof(b) / sizeof(b[0]);
 
-   double *res = mul_matrix<double>((double *) a, sizex_a, sizey_a, (double *) b, sizex_b, sizey_b);
+   double *res = mul_matrix<double>((double *) a, sizex_a, sizey_a, (double *) a, sizex_a, sizey_a);
 
    for(int i = 0; i < sizey_a; i++){
-     for(int j = 0; j < sizex_b; j++){
-       cout << *((res + i * sizex_b) + j) << " ";
+     for(int j = 0; j < sizex_a; j++){
+       cout << *((res + i * sizex_a) + j) << " ";
      }
      cout << endl;
    }
+
+   bool s;
+   markov_step<double>(15, (double *) a, sizey_b, 2, 1, &s);
   
 } 
+
+
+/*
+
+ 1 - 2
+ 2 - 4
+ 3 - 8
+ 
+
+15 = 2 ** 3 + 2 ** 4 + 2 ** 2 + 2 ** 1
+ 
+ */
